4e79182709aa9a357a9388861f3e649c1f924415
Eugen Ciur <eugen@papermerge.com>
2023-06-01 09:11:47 +0200

    basic draft for renderning js file

--- roco/bin/cli.py
+++ roco/bin/cli.py
@@ -2,16 +2,11 @@ import click
 
 from roco.main import generate_runtime_config
 
+
 @click.command()
-@click.option(
-    '-o',
-    '--output-file',
-    default='papermerge_runtime_config.js',
-)
-def cli(output_file: str):
-    generate_runtime_config(
-        output_file=output_file
-    )
+def cli():
+    output = generate_runtime_config()
+    click.echo(output)
 
 
 if __name__ == "__main__":
--- roco/main.py
+++ roco/main.py
@@ -1,2 +1,36 @@
-def generate_runtime_config(output_file: str) -> None:
-    pass
+import pathlib
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+
+env = Environment(
+    loader=PackageLoader("roco"),
+    autoescape=select_autoescape()
+)
+
+
+def get_template():
+    cur_dir = pathlib.Path(__file__).parent.resolve()
+    template_path = cur_dir / "templates"
+
+    return env.get_template(
+        name="runtime-config.js.jinja2",
+        parent=str(template_path)
+    )
+
+
+def get_context():
+    """Builds context dictionary from environment variables"""
+
+    return dict(
+        internal_token_url="one",
+        redirect_uri="two",
+        google_client_id="abc",
+        google_authorize_url="123"
+    )
+
+
+def generate_runtime_config() -> str:
+    template = get_template()
+    context = get_context()
+
+    return template.render(context)
