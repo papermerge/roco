f9712f58a10bd295bf71fb5d069ef402b485cd8b
Eugen Ciur <eugen@papermerge.com>
2023-06-04 07:10:46 +0200

    move redirect uri as part of the provider

--- roco/config.py
+++ roco/config.py
@@ -1,50 +1,51 @@
-from pydantic import BaseSettings, root_validator, validator
+from pydantic import BaseSettings, root_validator
 
 
 class Settings(BaseSettings):
 
-    internal_token_url: str | None = None
-    redirect_uri: str | None = None
-
     google_client_id: str | None = None
     google_authorize_url: str | None = None
+    google_redirect_uri: str | None = None
     google_scope: str = 'openid email'
 
     github_client_id: str | None = None
     github_authorize_url: str | None = None
+    github_redirect_uri: str | None = None
     github_scope: str = 'openid email'
-
-    # will be true if both `internal_token_url` and `redirect_uri`
-    # are non-empty
-    oauth2: bool = False
-
-    @validator("oauth2", always=True)
-    def validator_oauth2(cls, value, values):
-        """
-        oauth2 field will be True if both internal_token_url
-        and redirect_uri are non-empty
-        """
-        if values["internal_token_url"] and values["redirect_uri"]:
-            return True
-
-        return False
 
     @root_validator
     def check_google_params(cls, values):
-        two_values = [
+        three_values = [
             values.get('google_client_id'),
-            values.get('google_authorize_url')
+            values.get('google_authorize_url'),
+            values.get('google_redirect_uri')
         ]
-        count = len([v for v in two_values if v])
+        count = len([v for v in three_values if v])
 
-        if count not in (0, 2):
+        if count not in (0, 3):
             raise ValueError(
-                'google_client_id and google_authorize_url should be'
-                ' either both absent or both present'
+                'google_client_id, google_authorize_url and google_redirect_uri'
+                ' should be either all absent or all present'
             )
 
         return values
 
+    @root_validator
+    def check_github_params(cls, values):
+        three_values = [
+            values.get('github_client_id'),
+            values.get('github_authorize_url'),
+            values.get('github_redirect_uri')
+        ]
+        count = len([v for v in three_values if v])
+
+        if count not in (0, 3):
+            raise ValueError(
+                'github_client_id, github_authorize_url and github_redirect_uri'
+                ' should be either all absent or all present'
+            )
+
+        return values
 
     class Config:
         env_prefix = 'PAPERMERGE__AUTH__'
--- tests/test_generate_runtime_config.py
+++ tests/test_generate_runtime_config.py
@@ -7,15 +7,14 @@ from roco.main import generate_runtime_config
 
 def test_only_google_client_id_provided():
     """
-    Either none or both `google_client_id` and `google_authorize_url`
-    should be supplied.
+    Either none or all `google_client_id`, `google_authorize_url`,
+    `google_redirect_uri` should be supplied.
     In this scenario only `google_client_id` is provided -> validation
     error is expected
     """
-    os.environ["PAPERMERGE__AUTH__REDIRECT_URI"] = "abc/callback"
-    os.environ["PAPERMERGE__AUTH__INTERNAL_TOKEN_URL"] = "abc/api/token"
     os.environ["PAPERMERGE__AUTH__GOOGLE_CLIENT_ID"] = "google123"
     os.environ["PAPERMERGE__AUTH__GOOGLE_AUTHORIZE_URL"] = ''
+    os.environ["PAPERMERGE__AUTH__GOOGLE_REDIRECT_URI"] = ''
 
     with pytest.raises(ValidationError):
         generate_runtime_config()
@@ -23,15 +22,14 @@ def test_only_google_client_id_provided():
 
 def test_only_google_authorize_url_provided():
     """
-    Either none or both `google_client_id` and `google_authorize_url`
-    should be supplied.
+    Either none or all `google_client_id`, `google_authorize_url`,
+    'google_redirect_uri' should be supplied.
     In this scenario only `authorize_url` is provided -> validation
     error is expected
     """
-    os.environ["PAPERMERGE__AUTH__REDIRECT_URI"] = "abc/callback"
-    os.environ["PAPERMERGE__AUTH__INTERNAL_TOKEN_URL"] = "abc/api/token"
     os.environ["PAPERMERGE__AUTH__GOOGLE_AUTHORIZE_URL"] = "some-google-url"
     os.environ["PAPERMERGE__AUTH__GOOGLE_CLIENT_ID"] = ''
+    os.environ["PAPERMERGE__AUTH__GOOGLE_REDIRECT_URI"] = ''
 
     with pytest.raises(ValidationError):
         generate_runtime_config()
@@ -42,28 +40,28 @@ def test_none_of_the_google_oauth2_fields_is_present():
     In scenario no google oauth2 fields are present. Such scenario
     is valid.
     """
-    os.environ["PAPERMERGE__AUTH__REDIRECT_URI"] = "abc/callback"
-    os.environ["PAPERMERGE__AUTH__INTERNAL_TOKEN_URL"] = "abc/api/token"
     os.environ["PAPERMERGE__AUTH__GOOGLE_AUTHORIZE_URL"] = ''
     os.environ["PAPERMERGE__AUTH__GOOGLE_CLIENT_ID"] = ''
+    os.environ["PAPERMERGE__AUTH__GOOGLE_REDIRECT_URI"] = ''
 
     result = generate_runtime_config()
-    assert "redirect_uri" in result
+    assert "window" in result
 
 
 def test_both_of_the_google_oauth2_fields_is_present():
     """
-    In this scenario both google oauth2 fields i.e.
-    `google_authorize_url` and `google_client_id` are present.
-    Such scenario is obviously valid.
+    In this scenario all google oauth2 fields i.e.
+    `google_authorize_url`, `google_client_id`, ``google_redirect_uri`
+    are present. Such scenario is obviously valid.
     """
-    os.environ["PAPERMERGE__AUTH__REDIRECT_URI"] = "abc/callback"
-    os.environ["PAPERMERGE__AUTH__INTERNAL_TOKEN_URL"] = "abc/api/token"
+
     os.environ["PAPERMERGE__AUTH__GOOGLE_AUTHORIZE_URL"] = 'some-google-url'
     os.environ["PAPERMERGE__AUTH__GOOGLE_CLIENT_ID"] = 'some-google-client-id'
+    os.environ["PAPERMERGE__AUTH__GOOGLE_REDIRECT_URI"] = "abc/callback"
 
     result = generate_runtime_config()
 
     assert "some-google-url" in result
     assert "some-google-client-id" in result
+    assert "abc/callback" in result
 
